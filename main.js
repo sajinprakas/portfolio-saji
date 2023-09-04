5bbdff7da2ab1abacdc310b4d38d631d9963def0
sajinprakas <sajinp297@gmail.com>
2023-09-04 23:03:00 +0530

    part-5 update

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-document.querySelectorAll(".reveal")
-.forEach(function(elem){
+function revealTospan() {
+    document.querySelectorAll(".reveal")
+    .forEach(function(elem){
     var parent = document.createElement("span");
     var child = document.createElement("span");
 
@@ -12,38 +13,42 @@ document.querySelectorAll(".reveal")
     elem.innerHTML = "";
     elem.appendChild(parent);
 });
-
-function revealTospan() {
-
 }
 
-revealTospan();
+function valueSetters() {
+    gsap.set("#nav a",{ y: "-100%" , opacity: 0});
+    gsap.set("#home .parent .child" ,{ y: "100%"})
+    gsap.set("#home .row img" ,{opacity: 0})
 
+    document.querySelectorAll("#Visual>g").forEach(function (e) {
+        var character = e.childNodes[0].childNodes[0];
 
-var tl = gsap.timeline();
+        character.style.strokeDasharray = character.getTotalLength() + 'px';
+        character.style.strokeDashoffset = character.getTotalLength() + 'px';
+})
+}
+
+function loaderAnimation(){
+    var tl = gsap.timeline();
 
 tl
-.from(".child span", {
+.from("#loader .child span", {
     x: "100",
     stagger: .2,
     duration: 1.4,
     ease: Power3.easeInOut
-
 })
 
-tl
-.to(".parent .child", {
-    y: "-100%",
+.to("#loader .parent .child", {
+    y: "-110%",
     duration: 1,
     ease: Circ.easeInOut
-
 })
 
 .to("#loader", {
     height: 0,
     duration: 1,
     ease: Circ.easeInOut
-
 })
 
 .to("#green", {
@@ -52,12 +57,53 @@ tl
     duration: 1,
     delay: -1,
     ease: Circ.easeInOut
-
 })
 
 .to("#green", {
     height: "0%",
     delay: -.3,
-    ease: Circ.easeInOut
-
+    ease: Circ.easeInOut, 
+    onComplete: function() {
+        animateHomepage();
+    }
 })
+}
+
+function animateHomepage() {
+
+    var tl = gsap.timeline();
+     tl
+     .to("#nav a" ,{
+        y: 0,
+        opacity: 1,
+        stagger:.05,
+        ease: Expo.easeInOut
+     })
+     .to("#home .parent .child" ,{
+        y: 0,
+        stagger:.1,
+        duration:2,
+        ease: Expo.easeInOut
+     })
+     .to("#home .row img" ,{
+        opacity: 1,
+        ease: Expo.easeInOut,
+        delay:-.5,
+        onComplete: function() {
+            animateSvg();
+        }
+     })
+}
+
+function animateSvg() {
+
+    gsap.to("#Visual>g>g>path, #Visual>g>g>polyline", {
+        strokeDashoffset:0,
+        duration:2,
+        ease:Expo.easeInOut,
+        delay:-.5
+    })
+}
+revealTospan();
+valueSetters();
+loaderAnimation();
